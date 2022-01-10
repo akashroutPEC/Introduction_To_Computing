33936b88b1d329b409dc7d5e01811a124b747c16
akashroutPEC <96916484+akashroutPEC@users.noreply.github.com>
2022-01-10 15:11:56 +0530

    Add files via upload

---
+++ asg1_21103080_output.py
@@ -0,0 +1,26 @@
+Python 3.9.7 (tags/v3.9.7:1016ef3, Aug 30 2021, 20:19:38) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+========== RESTART: D:\PYTHON SEMESTER I\ASSIGNMENT 1\asg1_21103080.py =========
+Enter 1st number: 10
+Enter 2nd number: 12
+Enter 3rd number: 14
+12.0
+Enter your income to the nearest penny: 60000.00
+Enter number of dependents: 2
+$ 8800.0
+Enter your Student ID: 21103080
+Enter your name: Akash
+Enter your gender: M for Male, F for Female, U for Unknown: M
+Enter your course: CSE
+Enter your CGPA: 9.8
+['21103080', 'Akash', 'M', 'CSE', 9.8]
+Enter marks of student 1: 36
+Enter marks of student 2: 58
+Enter marks of student 3: 69
+Enter marks of student 4: 74
+Enter marks of student 5: 52
+[36, 52, 58, 69, 74]
+['Red', 'Green', 'White', 'Pink', 'Yellow']
+['Red', 'Green', 'White', 'Purple', 'Yellow']
+>>> 
