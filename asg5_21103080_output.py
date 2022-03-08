3fc2cbde606179b18b39c3bc0f47581e8150abcc
akashroutPEC <96916484+akashroutPEC@users.noreply.github.com>
2022-03-08 11:37:22 +0530

    Add files via upload

---
+++ asg5_21103080_output.py
@@ -0,0 +1,27 @@
+Python 3.9.7 (tags/v3.9.7:1016ef3, Aug 30 2021, 20:19:38) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+========== RESTART: D:\PYTHON SEMESTER I\ASSIGNMENT 5\asg5_21103080.py =========
+Enter size of list: 4
+Enter marks: 23
+Enter marks: 12
+Enter marks: 33
+Enter marks: 34
+[12, 23, 33, 34]
+Enter size of array: 11
+Enter element 4
+Enter element 5
+Enter element 2
+Enter element 4
+Enter element 5
+Enter element 6
+Enter element 2
+Enter element 3
+Enter element 4
+Enter element 5
+Enter element 7
+[2, 2, 3, 4, 4, 4, 5, 5, 5, 6, 7]
+Enter element to be searched and counted: 4
+Element is present at index 5
+Number of occurrences of element is 3
+>>> 
